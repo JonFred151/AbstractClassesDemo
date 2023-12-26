1676831705d5a65fdabf33b39abe0bc57ead7972
Jon Frederick <jonFred151@gmail.com>
2023-12-26 12:08:56 -0600

    Completed

---
+++ AbstractClassesDemo/Motorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesDemo
+{
+    internal class Motorcycle: Vehicle
+    {
+        public bool HasSidecart { get; set; }
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("The motorcycle is driving");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("Motorcycle Virtual drive method");
+        }
+    }
+}
---
+++ AbstractClassesDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using AbstractClassesDemo;
+using System;
+namespace ConsoleUI 
+{
+    class Progrom
+    {
+        static void Main(string[] args)
+        {
+            #region vehicles
+
+            var vehicles = new List<Vehicle>();
+             
+            var car1 = new Car()
+            { 
+                HasTrunk = true,
+                Year = "2000",
+                Make = "Ford",
+                Model= "F-150"
+            };
+            var motorcycle1 = new Motorcycle()
+            { 
+                HasSidecart = true,
+                Year = "2001",
+                Make = "Kawasaki",
+                Model = "Ninja"
+            };
+            Vehicle vehicle1 = new Car() 
+            { 
+                Make = "Ford", 
+                Year = "2014",
+                Model = "Focus"
+            };
+            Vehicle vehicle2 = new Motorcycle()
+            { 
+                Year = "2014",
+                Make = "Harley" ,
+                Model = "Davidson"
+            };
+            
+            vehicles.Add(car1);
+            vehicles.Add(motorcycle1);
+            vehicles.Add(vehicle1);
+            vehicles.Add(vehicle2);
+
+            foreach (var Vehicle in vehicles)
+            {
+                Console.WriteLine($"Year: {Vehicle.Year} \nMake: {Vehicle.Make} \nModel: {Vehicle.Model}");
+               
+                Vehicle.DriveAbstract();
+                Vehicle.DriveVirtual();
+                Console.WriteLine(" ");
+               
+            }
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
